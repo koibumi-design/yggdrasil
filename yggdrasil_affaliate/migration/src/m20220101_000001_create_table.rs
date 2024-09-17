b861cf1734413f92d81d65e2325165ca7f9b8128
haruki-nikaidou <meteo.haru@gmail.com>
2024-09-17 17:40:49 -0400

    finish migration for affiliate module

--- yggdrasil_affaliate/migration/src/m20220101_000001_create_table.rs
+++ yggdrasil_affaliate/migration/src/m20220101_000001_create_table.rs
@@ -1,25 +1,99 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::{prelude::*};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
-#[async_trait::async_trait]
-impl MigrationTrait for Migration {
-    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-    }
-
-    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-    }
+#[derive(DeriveIden)]
+enum AffGraph {
+    #[sea_orm(iden = "ygg_affiliate__graph")]
+    Table,
+    Id,
+    From,
+    To,
+    Reward,
+    Rate,
+    CreatedAt,
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum AffStat {
+    #[sea_orm(iden = "ygg_affiliate__statistics")]
     Table,
-    Id,
-    Title,
-    Text,
+    UserId,
+    Total,
+    Withdrawn,
+    CountReferrals,
+    Rate,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+            Table::create()
+                .table(AffGraph::Table)
+                .if_not_exists()
+                .col(ColumnDef::new(AffGraph::Id).integer().not_null().auto_increment().primary_key())
+                .col(ColumnDef::new(AffGraph::From).uuid().not_null())
+                .col(ColumnDef::new(AffGraph::To).uuid().not_null())
+                .col(ColumnDef::new(AffGraph::Reward).float().not_null())
+                .col(ColumnDef::new(AffGraph::Rate).float().not_null())
+                .col(ColumnDef::new(AffGraph::CreatedAt).timestamp().not_null().default("CURRENT_TIMESTAMP"))
+                .to_owned()
+        ).await?;
+        manager
+            .create_table(
+            Table::create()
+                .table(AffStat::Table)
+                .if_not_exists()
+                .col(ColumnDef::new(AffStat::UserId).uuid().not_null().primary_key())
+                .col(ColumnDef::new(AffStat::Total).float().not_null())
+                .col(ColumnDef::new(AffStat::Withdrawn).float().not_null())
+                .col(ColumnDef::new(AffStat::CountReferrals).integer().not_null())
+                .col(ColumnDef::new(AffStat::Rate).float().not_null())
+                .to_owned()
+        ).await?;
+        manager.create_index(
+            Index::create()
+                .table(AffGraph::Table)
+                .name("ygg_affiliate__from_index")
+                .col(AffGraph::From)
+                .to_owned()
+        ).await?;
+        manager.create_index(
+            Index::create()
+                .table(AffGraph::Table)
+                .name("ygg_affiliate__to_index")
+                .col(AffGraph::To)
+                .to_owned()
+        ).await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.drop_index(
+            Index::drop()
+                .name("ygg_affiliate__from_index")
+                .table(AffGraph::Table)
+                .to_owned()
+        ).await?;
+        manager.drop_index(
+            Index::drop()
+                .name("ygg_affiliate__to_index")
+                .table(AffGraph::Table)
+                .to_owned()
+        ).await?;
+        manager.drop_table(
+            Table::drop()
+                .table(AffGraph::Table)
+                .to_owned()
+        ).await?;
+        manager.drop_table(
+            Table::drop()
+                .table(AffStat::Table)
+                .to_owned()
+        ).await?;
+        Ok(())
+    }
 }
