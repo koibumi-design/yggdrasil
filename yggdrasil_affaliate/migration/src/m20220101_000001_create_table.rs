10c8483e796f4eccb875ca01d97cc210eb037671
haruki-nikaidou <meteo.haru@gmail.com>
2024-09-17 17:07:42 -0400

    migration

---
+++ yggdrasil_affaliate/migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,25 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+    }
+}
+
+#[derive(DeriveIden)]
+enum Post {
+    Table,
+    Id,
+    Title,
+    Text,
+}
